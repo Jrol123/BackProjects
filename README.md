94dc78fb206b300c631907abeccd57888ff2c5d5
Artemii <angap4@gmail.com>
2024-03-07 16:26:41 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# BackProjects
+ 
---
+++ p2/src/main/java/edu/phoenix/dao/Database.java
@@ -0,0 +1,39 @@
+package edu.phoenix.dao;
+
+import edu.phoenix.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Database {
+    public static List<User> dataBase = new ArrayList<>();
+
+    public static void addUser(String name, String login, String password) {
+        dataBase.add(new User(name, login, password));
+    }
+
+    public static User getUser(String login, String password) {
+        // TODO: Добавить throw exception
+        if (password.isEmpty()) {
+            return dataBase.stream()
+                    .filter(obj -> obj.getLogin().equals(login))
+                    .findFirst()
+                    .orElse(null);
+        }
+        return dataBase.stream()
+                .filter(obj -> obj.getLogin().equals(login) &&
+                        obj.getPassword().equals(password))
+                .findFirst()
+                .orElse(null);
+    }
+
+    public static void deleteUser(String login) {
+        dataBase.remove(getUser(login, ""));
+    }
+
+    public static void updateUser(User user) {
+        dataBase.remove(getUser(user.getLogin(), user.getPassword()));
+        dataBase.add(user);
+    }
+
+}
