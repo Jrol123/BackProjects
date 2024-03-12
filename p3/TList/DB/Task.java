f6e55445bd11f9da2d7bdb99306049ad02701346
Artemii <angap4@gmail.com>
2024-03-12 14:53:23 +1000

    p3 start

---
+++ p3/TList/DB/Task.java
@@ -0,0 +1,13 @@
+package TList.DB;
+
+public class Task {
+    private int id;
+    private String name;
+    private String inside;
+
+    public Task(int id, String name, String inside) {
+        this.id = id;
+        this.name = name;
+        this.inside = inside;
+    }
+}
---
+++ p3/TList/DB/Tasklist.java
@@ -0,0 +1,17 @@
+package TList.DB;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tasklist {
+    public static List<Task> dataBase = new ArrayList<>();
+
+    public static void addTask(String name, String inside){
+        dataBase.add(new Task(dataBase.size(), name, inside));
+        GET
+    }
+
+    public static Task returnTask(int id){
+        return dataBase.get(id);
+    }
+}
