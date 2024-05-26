4247229048d06a6c847e74d32cb23b108cfcecb2
Artemii <angap4@gmail.com>
2024-05-26 13:21:57 +1000

    Simplify Exceptions

--- project/src/main/java/com/example/project/exception/UsersNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.example.project.exception;
-
-import org.springframework.http.HttpStatus;
-
-public class UsersNotFoundException extends ApiException{
-    public UsersNotFoundException(String message) {
-        super(HttpStatus.NOT_FOUND, message);
-    }
-}
