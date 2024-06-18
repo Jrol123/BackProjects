2e9e196a4f672740ffa2f64c01cd97029de990bb
Artemii <angap4@gmail.com>
2024-06-18 20:56:08 +1000

    rm useless data

--- p4/src/main/java/com/example/p4/repository/test.java
+++
@@ -1,7 +0,0 @@
-package com.example.p4.repository;
-
-import com.example.p4.entity.test_table;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface test extends JpaRepository<test_table, Long> {
-}
