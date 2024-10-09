af33d52677d88fcfd8104a1307019a513451d62a
Artemii <angap4@gmail.com>
2024-10-09 23:18:24 +1000

    fix lab1

--- p1/TestCase.java
+++ p1/TestCase.java
@@ -1,5 +1,5 @@
 public class TestCase {
-    public static void getterTest(String[] args) {
+    public static void main(String[] args) {
         Cat mark = new Cat("mark", 1, "Artemii");
         System.out.print(mark.getName());
         System.out.print(mark.getAge());
